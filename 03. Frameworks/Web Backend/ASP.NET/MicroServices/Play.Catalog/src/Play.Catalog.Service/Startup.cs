5535aa8fd7be7e52e1862e36ae716398380a373f
hyonkok2 <64830380+hyonkok2@users.noreply.github.com>
2023-05-20 01:21:45 +0900

    Monog DB Repository 등록 기능 추출

--- 03. Frameworks/Web Backend/ASP.NET/MicroServices/Play.Catalog/src/Play.Catalog.Service/Startup.cs
+++ 03. Frameworks/Web Backend/ASP.NET/MicroServices/Play.Catalog/src/Play.Catalog.Service/Startup.cs
@@ -16,6 +16,7 @@ using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
 using MongoDB.Driver;
 using Paly.Catalog.Service.Entities;
+using Paly.Catalog.Service.Repositories;
 using Play.Catalog.Service.Repositories;
 using Play.Catalog.Service.Settings;
 
@@ -34,23 +35,10 @@ namespace Play.Catalog.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
-            BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
-
             serviceSettings = Configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>();
 
-            services.AddSingleton(serviceProvider =>
-            {
-                var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                var mongoClient = new MongoClient(mongoDbSettings.ConnectionString);
-                return mongoClient.GetDatabase(serviceSettings.ServiceName);
-            });
-
-            services.AddSingleton<IRepository<Item>>(serviceProvider =>
-            {
-                var database = serviceProvider.GetService<IMongoDatabase>();
-                return new MongoRepository<Item>(database, "items");
-            });
+            services.AddMongo()
+                    .AddMongoRepository<Item>("items");
 
             services.AddControllers(options =>
             {
