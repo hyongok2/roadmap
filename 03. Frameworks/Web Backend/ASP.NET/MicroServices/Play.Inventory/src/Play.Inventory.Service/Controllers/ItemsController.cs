a66b0c31b8723d438771939b453ed90ec672b3b2
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-05-20 19:56:20 +0900

    Inventory Item Service 프로젝트 추가

---
+++ 03. Frameworks/Web Backend/ASP.NET/MicroServices/Play.Inventory/src/Play.Inventory.Service/Controllers/ItemsController.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Play.Common;
+using Play.Inventory.Service.Dtos;
+using Play.Inventory.Service.Entities;
+
+namespace Play.Inventory.Service.Controller
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly IRepository<InventoryItem> itemsRepository;
+
+        public ItemsController(IRepository<InventoryItem> itemsRepository)
+        {
+            this.itemsRepository = itemsRepository;
+        }
+        [HttpGet]
+        public async Task<ActionResult<InventoryItemDto>> GetAsync(Guid userId)
+        {
+            if (userId == Guid.Empty)
+            {
+                return BadRequest();
+            }
+
+            var items = (await itemsRepository.GetAllAsync(item => item.UserId == userId))
+                                .Select(item => item.AsDto());
+
+            if (items == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(items);
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult> PostAsync(GrantItemsDto grantItemsDto)
+        {
+            var inventoryItem = await itemsRepository.GetAsync(
+                item => item.UserId == grantItemsDto.UserId && item.CatalogItemId == grantItemsDto.CatalogItemId);
+
+            if (inventoryItem == null)
+            {
+                inventoryItem = new InventoryItem
+                {
+                    CatalogItemId = grantItemsDto.CatalogItemId,
+                    UserId = grantItemsDto.UserId,
+                    Quantity = grantItemsDto.Quantity,
+                    AcquiredDate = DateTimeOffset.UtcNow
+                };
+
+                await itemsRepository.CreateAsync(inventoryItem);
+            }
+            else
+            {
+                inventoryItem.Quantity += grantItemsDto.Quantity;
+                await itemsRepository.UpdateAsync(inventoryItem);
+            }
+
+            return Ok();
+        }
+    }
+}
