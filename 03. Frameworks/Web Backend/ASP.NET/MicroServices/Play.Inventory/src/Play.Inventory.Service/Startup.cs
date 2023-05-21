ba2e788bbc4cf1e09aa5bb3ec494c79c85288c29
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-05-21 13:50:40 +0900

    Test for circuit Breaker Pattern

--- 03. Frameworks/Web Backend/ASP.NET/MicroServices/Play.Inventory/src/Play.Inventory.Service/Startup.cs
+++ 03. Frameworks/Web Backend/ASP.NET/MicroServices/Play.Inventory/src/Play.Inventory.Service/Startup.cs
@@ -47,6 +47,23 @@ namespace Play.Inventory.Service
                     .LogWarning($"Delaying for {timespan.TotalSeconds} seconds, then making retry {retryAttempt}");
                 }
             ))
+            .AddTransientHttpErrorPolicy(builder => builder.Or<TimeoutRejectedException>().CircuitBreakerAsync(
+                3,
+                TimeSpan.FromSeconds(15),
+                onBreak: (outcome, timespan) =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?
+                    .LogWarning($"Opening the circuit for {timespan.TotalSeconds} second...");
+                },
+                onReset: () =>
+                {
+                    var serviceProvider = services.BuildServiceProvider();
+                    serviceProvider.GetService<ILogger<CatalogClient>>()?
+                    .LogWarning($"Closing the circuit...");
+                }
+
+            ))
             .AddPolicyHandler(Policy.TimeoutAsync<HttpResponseMessage>(1));
 
             services.AddControllers();
