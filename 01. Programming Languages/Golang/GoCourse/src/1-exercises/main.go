4e8b5c8f6e237377c0007cad947bea789cbfeeca
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-05-25 22:59:28 +0900

    Go 기본 문법 학습

--- 01. Programming Languages/Golang/GoCourse/src/1-exercises/main.go
+++ 01. Programming Languages/Golang/GoCourse/src/1-exercises/main.go
@@ -4,4 +4,35 @@ import "fmt"
 
 func main() {
 	fmt.Println("Les't Start Go Programming. Today is 2023.05.24")
+
+	day1()
+}
+
+func day1() {
+
+	var smsSendingLimit int
+	var cost float64
+	var hasIt bool
+	var username string
+	fmt.Printf(
+		"%v %f %v %q \n",
+		smsSendingLimit, cost, hasIt, username)
+
+	//empty := ""
+	//numberCars := 20
+	temperature := 34
+	// var isFunny = true
+	temperatureFloat := float64(temperature)
+	fmt.Printf("numberCars TYPE : %T %v", temperatureFloat, temperatureFloat)
+
+	messageLen := 100
+	maxMessageLen := 200
+	fmt.Println("Trying to send a message of length:", messageLen)
+
+	if messageLen <= maxMessageLen {
+		fmt.Println("Message sent")
+	} else {
+		fmt.Println("Message not sent")
+	}
+
 }
