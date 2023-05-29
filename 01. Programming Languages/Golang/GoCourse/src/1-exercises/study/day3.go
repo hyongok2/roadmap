0aaad430591ba2be362ad843dd08e6ce485da617
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-05-29 12:28:25 +0900

    Go 3일차 Error Handling 학습

---
+++ 01. Programming Languages/Golang/GoCourse/src/1-exercises/study/day3.go
@@ -0,0 +1,57 @@
+package study
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func Day3() {
+
+	user, err := getUser()
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(user)
+
+	i, err := strconv.Atoi("5r")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("value : %v \n", i)
+
+	val, err := divide(34.123, 0)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Printf("Result : %v \n", val)
+
+}
+
+func getUser() (user string, e error) {
+
+	return "사람", errors.New("이것은 에러")
+}
+
+type divideError struct {
+	dividend float64
+}
+
+func (d divideError) Error() string {
+	return fmt.Sprintf("Can not divide %v By Zero", d.dividend)
+}
+
+func divide(dividend, divisor float64) (float64, error) {
+
+	if divisor == 0 {
+		return 0.0, divideError{dividend}
+	}
+
+	return dividend / divisor, nil
+}
