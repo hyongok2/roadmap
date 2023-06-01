ae9bd86160f5302d23cf58ab327260d4f1af20d2
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-06-01 23:06:08 +0900

    Unity Player Movement Test

--- 09. Game Development/Unity/OrbJumper/Assets/Scripts/PlayerMovement.cs
+++ 09. Game Development/Unity/OrbJumper/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody rb;
+
+    public float ForwardForce;
+    public float SideForce;
+
+    // Start is called before the first frame update    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, ForwardForce * Time.deltaTime);
+        }
+
+        if (Input.GetKey("z"))
+        {
+            rb.AddForce(0, 0, -ForwardForce * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(SideForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-SideForce * Time.deltaTime, 0, 0);
+        }
+
     }
 }
