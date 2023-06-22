a646fa4450d1f31c22eb16bf4b296ac7aa20e5a6
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-06-22 22:48:06 +0900

    AVALONIA making volume arrow

--- 03. Frameworks/Desktop App/Avalonia/Tutorial by AngelSix/src/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/ViewModels/MainViewModel.cs
+++ 03. Frameworks/Desktop App/Avalonia/Tutorial by AngelSix/src/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/ViewModels/MainViewModel.cs
@@ -7,10 +7,12 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using Avalonia.Threading;
 using AvaloniaLoudnessMeter.DataModels;
 using AvaloniaLoudnessMeter.Sevices;
 using CommunityToolkit.Mvvm.Collections;
 using CommunityToolkit.Mvvm.Input;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 
 namespace AvaloniaLoudnessMeter.ViewModels
@@ -27,6 +29,12 @@ namespace AvaloniaLoudnessMeter.ViewModels
 
         [ObservableProperty]
         private bool _channelConfigurationListIsOpen = false;
+        
+        [ObservableProperty]
+        private double _volumePercentPosition;
+        
+        [ObservableProperty]
+        private double _volumeContainerSize;
         
         [ObservableProperty]
         private ObservableGroupedCollection<string, ChannelConfigurationItem> _channelConfigurations = default!;
@@ -62,11 +70,42 @@ namespace AvaloniaLoudnessMeter.ViewModels
         public MainViewModel(IAudioInterfaceService audioInterfaceService)
         {
             _audioInterfaceService = audioInterfaceService;
+
+            Initialize();
         }
-        
+
         public MainViewModel()
         {
             _audioInterfaceService = new DummyAudioInterfaceService();
-        } 
+            
+            Initialize();
+        }
+        
+        private void Initialize()
+        {
+            // temp code to move volume position
+            
+            var tick = 0;
+            var input = 0.0;
+
+            var tempTimer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1/60.0)
+            };
+
+            tempTimer.Tick += (sender, args) =>
+            {
+                tick++;
+
+                input = (double)tick / 20;
+
+                var scale = VolumeContainerSize;
+
+                VolumePercentPosition = (Math.Sin(input) + 1) / 2 * scale;
+            };
+            
+
+            tempTimer.Start();
+        }
     }
 }
--- 03. Frameworks/Desktop App/Avalonia/Tutorial by AngelSix/src/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Views/MainView.axaml.cs
+++ 03. Frameworks/Desktop App/Avalonia/Tutorial by AngelSix/src/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/AvaloniaLoudnessMeter/Views/MainView.axaml.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Avalonia;
 using Avalonia.Animation.Easings;
@@ -17,16 +18,33 @@ namespace AvaloniaLoudnessMeter.Views
         private readonly Control _channelConfigPopUp;
         private readonly Control _channelConfigButton;
         private readonly Control _mainGrid;
+        private readonly Control _volumeArrowContainer;
+
+        private Timer _sizingTimer;
 
         #endregion
         public MainView()
         {
             InitializeComponent();
             // DataContext = new MainViewModel();
+            
+            _sizingTimer = new Timer(t =>
+            {
+                Dispatcher.UIThread.InvokeAsync(() =>
+                {
+                    UpdateSizes();
+                });
+            });
 
             _channelConfigButton = this.FindControl<Control>("ChannelConfigurationButton") ?? throw new Exception("Cannot find Channel Configuration Button by name");
             _channelConfigPopUp = this.FindControl<Control>("ChannelConfigurationPopUp") ?? throw new Exception("Cannot find Channel Configuration Popup by name");
             _mainGrid = this.FindControl<Control>("MainGrid") ?? throw new Exception("Cannot find Main Grid by name");
+            _volumeArrowContainer= this.FindControl<Control>("VolumeArrowContainer") ?? throw new Exception("Cannot find Volume Arrow Container by name");
+        }
+
+        private void UpdateSizes()
+        {
+            ((MainViewModel)DataContext).VolumeContainerSize = _volumeArrowContainer.Bounds.Height;
         }
 
         protected override async void OnLoaded()
@@ -38,7 +56,9 @@ namespace AvaloniaLoudnessMeter.Views
         public override void Render(DrawingContext context)
         {
             base.Render(context);
-            
+
+            _sizingTimer.Change(100, int.MaxValue);
+          
             // Main Gird 기준 버튼의 상대 위치를 가져옴.
             var position = _channelConfigButton.TranslatePoint(new Point(), _mainGrid) ??
                            throw new Exception("Cannot get TranslatePoint from Configuration Button");
