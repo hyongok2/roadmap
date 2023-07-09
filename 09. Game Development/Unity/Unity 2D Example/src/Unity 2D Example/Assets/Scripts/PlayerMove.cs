4b80617c05ad864e764ab16f60150d0a084c4914
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-07-09 17:11:15 +0900

    Unity 2D Player animation handling

--- 09. Game Development/Unity/Unity 2D Example/src/Unity 2D Example/Assets/Scripts/PlayerMove.cs
+++ 09. Game Development/Unity/Unity 2D Example/src/Unity 2D Example/Assets/Scripts/PlayerMove.cs
@@ -1,18 +1,48 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    public float maxSpeed = 3f;
+    public float maxSpeed;
 
     Rigidbody2D body;
+    SpriteRenderer spriteRenderer;
+    Animator animator;
 
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        //Stop Speed SetUp
+        if(Input.GetButtonUp("Horizontal"))
+        {            
+            body.velocity = new Vector2(body.velocity.normalized.x * 0.5f, body.velocity.y);
+        }
+
+        if(Input.GetButtonDown("Horizontal"))
+        {
+            spriteRenderer.flipX= Input.GetAxisRaw("Horizontal") == -1;
+        }
+
+        //Walking Animation
+        if(Mathf.Abs(body.velocity.x) < 0.3f)
+        {
+            animator.SetBool("isWalking",false);
+        }
+        else
+        {
+            animator.SetBool("isWalking", true);
+        }
+
     }
 
     // Update is called once per frame
