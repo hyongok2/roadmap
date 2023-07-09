92cf4cd6cb3b4192d8af792820fe6adc2217951f
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-07-09 21:22:40 +0900

    Unity Player Jump Motion

--- 09. Game Development/Unity/Unity 2D Example/src/Unity 2D Example/Assets/Scripts/PlayerMove.cs
+++ 09. Game Development/Unity/Unity 2D Example/src/Unity 2D Example/Assets/Scripts/PlayerMove.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     public float maxSpeed;
+    public float jumpPower;
 
     Rigidbody2D body;
     SpriteRenderer spriteRenderer;
@@ -22,8 +23,14 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetButtonDown("Jump") && animator.GetBool("isJumping") == false)
+        {
+            body.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            animator.SetBool("isJumping", true);
+        }
+
         //Stop Speed SetUp
-        if(Input.GetButtonUp("Horizontal"))
+        if (Input.GetButtonUp("Horizontal"))
         {            
             body.velocity = new Vector2(body.velocity.normalized.x * 0.5f, body.velocity.y);
         }
@@ -59,5 +66,22 @@ public class PlayerMove : MonoBehaviour
         {
             body.velocity = new Vector2(-maxSpeed, body.velocity.y);
         }
+
+        if(body.velocity.y < 0)
+        {
+            Debug.DrawRay(body.position, Vector3.down, Color.green);
+
+            RaycastHit2D rayHit = Physics2D.Raycast(body.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
+
+            if (rayHit.collider != null)
+            {
+                if (rayHit.distance < 0.5f)
+                {
+                    animator.SetBool("isJumping", false);
+                }
+            }
+        }
+
+
     }
 }
