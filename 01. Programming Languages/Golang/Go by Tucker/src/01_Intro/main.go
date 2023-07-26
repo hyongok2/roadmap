ffb1fb2b4b1f63b6e030688c35ec10a6f26dd408
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-07-26 22:02:53 +0900

    Go 변수 기본

--- 01. Programming Languages/Golang/Go by Tucker/src/01_Intro/main.go
+++ 01. Programming Languages/Golang/Go by Tucker/src/01_Intro/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hello go world!")
+	var a int = 10
+	var msg string = "hello"
+
+	a = 20
+	msg = "good morning"
+
+	fmt.Println(msg, a)
+
+	d := 12.2
+
+	fmt.Println(d)
+
+	var ff float64 = 3.14
+
+	var ii int = int(ff)
+
+	fmt.Println(ii)
+
 }
