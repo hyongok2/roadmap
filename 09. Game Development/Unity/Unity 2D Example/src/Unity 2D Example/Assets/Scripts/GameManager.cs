552f44c42a190fa47754541e585598ba34e52860
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-08-14 10:35:53 +0900

    Unity2D Game UI 구현

--- 09. Game Development/Unity/Unity 2D Example/src/Unity 2D Example/Assets/Scripts/GameManager.cs
+++ 09. Game Development/Unity/Unity 2D Example/src/Unity 2D Example/Assets/Scripts/GameManager.cs
@@ -1,6 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -12,6 +15,16 @@ public class GameManager : MonoBehaviour
 
     public GameObject[] Stages;
 
+    public Image[] UIHealth;
+    public TextMeshProUGUI UIPoint;
+    public TextMeshProUGUI UIStage;
+    public GameObject UIRestartButton;
+
+    private void Update()
+    {
+        UIPoint.text = (totalPoint + stagePoint).ToString();
+    }
+
     public void NextStage()
     {
         if(stageIndex < Stages.Length -1)
@@ -20,11 +33,17 @@ public class GameManager : MonoBehaviour
             stageIndex++;
             Stages[stageIndex].SetActive(true);
             PlayerReposition();
+
+            UIStage.text = "STAGE " + (stageIndex + 1).ToString();
         }
         else
         {
             Time.timeScale = 0;
             Debug.Log("Game Clear!");
+
+            TextMeshProUGUI btnText = UIRestartButton.GetComponentInChildren<TextMeshProUGUI>();
+            btnText.text = "Game Clear!";
+            UIRestartButton.SetActive(true);
         }
 
 
@@ -36,11 +55,19 @@ public class GameManager : MonoBehaviour
 
     public void HealthDown()
     {
-        if(health > 1) health--;
+        if (health > 1)
+        {
+            health--;
+            UIHealth[health].color = new Color(1, 0, 0, 0.2f);
+        }
         else
         {
+            UIHealth[0].color = new Color(1, 0, 0, 0.2f);
             player.OnDie();
             Debug.Log("플레이어가 죽었습니다.");
+            TextMeshProUGUI btnText = UIRestartButton.GetComponentInChildren<TextMeshProUGUI>();
+            btnText.text = "Retry?";
+            UIRestartButton.SetActive(true);
         }
     }
 
@@ -63,4 +90,9 @@ public class GameManager : MonoBehaviour
         player.VelocityZero();
     }
 
+    public void Restart()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene(0);
+    }
 }
