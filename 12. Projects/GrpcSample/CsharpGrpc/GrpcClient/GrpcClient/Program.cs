098db92cec9c98725a9c9a39496223d0ecfcf7ea
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2023-11-20 22:30:56 +0900

    gRPC C# Client Sample 추가

---
+++ 12. Projects/GrpcSample/CsharpGrpc/GrpcClient/GrpcClient/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcClient;
+
+Console.WriteLine("Hello, World!");
+
+var channel = GrpcChannel.ForAddress("http://127.0.0.1:50051");
+
+var client = new Calculator.CalculatorClient(channel);
+
+Number number = new Number();
+
+number.Value = 225;
+
+var response = client.SquareRoot(number);
+
+Console.WriteLine(response.Value); 
