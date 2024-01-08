a23610a999a09cc69c547478cee08ef8e5aee971
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2024-01-08 21:59:30 +0900

    python game tutorial 테트리스

---
+++ 01. Programming Languages/Python/pygame_tetris/code/main.py
@@ -0,0 +1,47 @@
+from settings import *
+from sys import exit
+
+from game import Game
+from score import Score
+from preview import Preview
+
+
+class Main:
+    def __init__(self):
+
+        # general setup
+        pygame.init()
+        self.display_surface = pygame.display.set_mode(
+            (WINDOW_WIDTH, WINDOW_HEIGHT))
+        self.clock = pygame.time.Clock()
+        pygame.display.set_caption('Tetris')
+
+        # components
+        self.game = Game()
+        self.score = Score()
+        self.preview = Preview()
+
+    def run(self):
+        while True:
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
+
+            # display
+            self.display_surface.fill(GRAY)
+
+            # components
+            self.game.run()
+            self.score.run()
+            self.preview.run()
+
+            # updating the game
+            pygame.display.update()
+            self.clock.tick()
+
+
+if __name__ == '__main__':
+    main = Main()
+    main.run()
