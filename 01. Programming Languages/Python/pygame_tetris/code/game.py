5819db41614101dfe65c7b5192ee72c7d88c5228
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2024-01-14 13:24:04 +0900

    pygame tetris - add user input and horizontal movement

--- 01. Programming Languages/Python/pygame_tetris/code/game.py
+++ 01. Programming Languages/Python/pygame_tetris/code/game.py
@@ -24,7 +24,8 @@ class Game:
 
         # timer
         self.timers = {
-            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down)
+            'vertical move': Timer(UPDATE_START_SPEED, True, self.move_down),
+            'horizontal move': Timer(MOVE_WAIT_TIME)
         }
 
         self.timers['vertical move'].activate()
@@ -49,9 +50,21 @@ class Game:
 
             self.surface.blit(self.line_surface, (0, 0))
 
+    def input(self):
+        keys = pygame.key.get_pressed()
+
+        if not self.timers['horizontal move'].active:
+            if (keys[pygame.K_LEFT]):
+                self.tetromino.move_horizontal(-1)
+                self.timers['horizontal move'].activate()
+            if (keys[pygame.K_RIGHT]):
+                self.tetromino.move_horizontal(1)
+                self.timers['horizontal move'].activate()
+
     def run(self):
 
         # update
+        self.input()
         self.timer_update()
         self.sprites.update()
 
@@ -78,6 +91,10 @@ class Tetromino:
     def move_down(self):
         for block in self.blocks:
             block.pos.y += 1
+
+    def move_horizontal(self, amount):
+        for block in self.blocks:
+            block.pos.x += amount
 
 
 class Block(pygame.sprite.Sprite):
