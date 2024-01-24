9c37b852c0df44492676f73592982884d956ab36
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2024-01-24 22:31:17 +0900

    pygame doom - 시작!

---
+++ 01. Programming Languages/Python/pygame_doom/main.py
@@ -0,0 +1,41 @@
+import pygame as pg
+import sys
+from settings import *
+from map import *
+
+
+class Game:
+    def __init__(self):
+        pg.init()
+        self.screen = pg.display.set_mode(RES)
+        self.clock = pg.time.Clock()
+        self.new_game()
+
+    def new_game(self):
+        self.map = Map(self)
+
+    def update(self):
+        pg.display.flip()
+        self.clock.tick(FPS)
+        pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
+
+    def draw(self):
+        self.screen.fill('black')
+        self.map.draw()
+
+    def check_event(self):
+        for event in pg.event.get():
+            if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
+                pg.quit()
+                sys.exit()
+
+    def run(self):
+        while True:
+            self.check_event()
+            self.update()
+            self.draw()
+
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
