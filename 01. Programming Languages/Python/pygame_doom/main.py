8c0629f3c4224b553f6c4079d173398c24eddd1a
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2024-01-27 18:33:47 +0900

    pygame doom - add player movement and raycasting

--- 01. Programming Languages/Python/pygame_doom/main.py
+++ 01. Programming Languages/Python/pygame_doom/main.py
@@ -2,6 +2,8 @@ import pygame as pg
 import sys
 from settings import *
 from map import *
+from player import *
+from raycasting import *
 
 
 class Game:
@@ -9,19 +11,25 @@ class Game:
         pg.init()
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
+        self.delta_time = 1
         self.new_game()
 
     def new_game(self):
         self.map = Map(self)
+        self.player = Player(self)
+        self.raycasting = RayCasting(self)
 
     def update(self):
+        self.player.update()
+        self.raycasting.update()
         pg.display.flip()
-        self.clock.tick(FPS)
+        self.delta_time = self.clock.tick(FPS)
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
         self.screen.fill('black')
         self.map.draw()
+        self.player.draw()
 
     def check_event(self):
         for event in pg.event.get():
