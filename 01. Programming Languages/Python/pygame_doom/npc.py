0001b61df4af2924f6dc266e3084ed482df589c3
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2024-02-05 21:36:32 +0900

    pygame doom - npc raycating

--- 01. Programming Languages/Python/pygame_doom/npc.py
+++ 01. Programming Languages/Python/pygame_doom/npc.py
@@ -19,11 +19,13 @@ class NPC(AnimatedSprite):
         self.accracy = 0.15
         self.alive = True
         self.pain = False
+        self.ray_cast_value = False
 
     def update(self):
         self.check_animation_time()
         self.get_sprite()
         self.run_logic()
+        # self.draw_ray_cast()
 
     def animate_pain(self):
         self.animate(self.pain_images)
@@ -31,7 +33,7 @@ class NPC(AnimatedSprite):
             self.pain = False
 
     def check_hit_in_npc(self):
-        if self.game.player.shot:
+        if self.ray_cast_value and self.game.player.shot:
             if HALF_WIDTH - self.sprite_half_width < self.screen_x < HALF_WIDTH + self.sprite_half_width:
                 self.game.sound.npc_pain.play()
                 self.game.player.shot = False
@@ -39,8 +41,83 @@ class NPC(AnimatedSprite):
 
     def run_logic(self):
         if self.alive:
+            self.ray_cast_value = self.ray_casting_player_npc()
             self.check_hit_in_npc()
             if self.pain:
                 self.animate_pain()
             else:
                 self.animate(self.idle_images)
+
+    @property
+    def map_pos(self):
+        return int(self.x), int(self.y)
+
+    def ray_casting_player_npc(self):
+        if self.game.player.map_pos == self.map_pos:
+            return True
+
+        wall_dist_v, wall_dist_h = 0, 0
+        player_dist_v, player_dist_h = 0, 0
+
+        ox, oy = self.game.player.pos
+        x_map, y_map = self.game.player.map_pos
+
+        ray_angle = self.theta
+
+        sin_a = math.sin(ray_angle)
+        cos_a = math.cos(ray_angle)
+
+        # horizontals
+        y_hor, dy = (y_map + 1, 1) if sin_a > 0 else (y_map - 1e-6, -1)
+        depth_hor = (y_hor - oy) / sin_a
+        x_hor = ox + depth_hor * cos_a
+
+        delta_depth = dy / sin_a
+        dx = delta_depth * cos_a
+
+        for i in range(MAX_DEPTH):
+            tile_hor = int(x_hor), int(y_hor)
+            if tile_hor == self.map_pos:
+                player_dist_h = depth_hor
+                break
+            if tile_hor in self.game.map.world_map:
+                wall_dist_h = depth_hor
+                break
+            x_hor += dx
+            y_hor += dy
+            depth_hor += delta_depth
+
+        # verticals
+        x_vert, dx = (x_map + 1, 1) if cos_a > 0 else (x_map - 1e-6, -1)
+
+        depth_vert = (x_vert - ox) / cos_a
+        y_vert = oy + depth_vert * sin_a
+
+        delta_depth = dx / cos_a
+        dy = delta_depth * sin_a
+
+        for i in range(MAX_DEPTH):
+            tile_vert = int(x_vert), int(y_vert)
+            if tile_vert == self.map_pos:
+                player_dist_v = depth_vert
+                break
+            if tile_vert in self.game.map.world_map:
+                wall_dist_v = depth_vert
+                break
+            x_vert += dx
+            y_vert += dy
+            depth_vert += delta_depth
+
+        player_dist = max(player_dist_h, player_dist_v)
+        wall_dist = max(wall_dist_h, wall_dist_v)
+
+        if 0 < player_dist < wall_dist or not wall_dist:
+            return True
+        return False
+
+    def draw_ray_cast(self):
+        pg.draw.circle(self.game.screen, 'red',
+                       (100 * self.x, 100 * self.y), 15)
+        if self.ray_casting_player_npc():
+            pg.draw.line(self.game.screen, 'orange', (100 * self.game.player.x,
+                         100 * self.game.player.y), (100 * self.x, 100 * self.y), 2)
