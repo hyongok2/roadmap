e0b8e13b3b31e31540484c79674ef295c22062f2
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2024-02-24 22:03:38 +0900

    pygame-doom npc add movement

--- 01. Programming Languages/Python/pygame_doom/main.py
+++ 01. Programming Languages/Python/pygame_doom/main.py
@@ -9,6 +9,7 @@ from sprite_object import *
 from object_handler import *
 from weapon import *
 from sound import *
+from pathfinding import *
 
 
 class Game:
@@ -18,6 +19,9 @@ class Game:
         self.screen = pg.display.set_mode(RES)
         self.clock = pg.time.Clock()
         self.delta_time = 1
+        self.global_trigger = False
+        self.global_event = pg.USEREVENT + 0
+        pg.time.set_timer(self.global_event, 40)
         self.new_game()
 
     def new_game(self):
@@ -28,6 +32,7 @@ class Game:
         self.object_handler = ObjectHandler(self)
         self.weapon = Weapon(self)
         self.sound = Sound(self)
+        self.pathfinding = PathFinding(self)
 
     def update(self):
         self.player.update()
@@ -39,17 +44,20 @@ class Game:
         pg.display.set_caption(f'{self.clock.get_fps() :.1f}')
 
     def draw(self):
-        # self.screen.fill('black')
-        self.object_renderer.darw()
-        self.weapon.draw()
-        # self.map.draw()
-        # self.player.draw()
+        self.screen.fill('black')
+        # self.object_renderer.darw()
+        # self.weapon.draw()
+        self.map.draw()
+        self.player.draw()
 
     def check_event(self):
+        self.global_trigger = False
         for event in pg.event.get():
             if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
                 pg.quit()
                 sys.exit()
+            elif event.type == self.global_event:
+                self.global_trigger = True
             self.player.single_fire_event(event)
 
     def run(self):
--- 01. Programming Languages/Python/pygame_doom/npc.py
+++ 01. Programming Languages/Python/pygame_doom/npc.py
@@ -20,12 +20,41 @@ class NPC(AnimatedSprite):
         self.alive = True
         self.pain = False
         self.ray_cast_value = False
+        self.frame_counter = 0
+        self.player_search_trigger = False
 
     def update(self):
         self.check_animation_time()
         self.get_sprite()
         self.run_logic()
-        # self.draw_ray_cast()
+        self.draw_ray_cast()
+
+    def movement(self):
+        next_pos = self.game.pathfinding.get_path(
+            self.map_pos, self.game.player.map_pos)
+        next_x, next_y = next_pos
+        pg.draw.rect(self.game.screen, 'blue',
+                     (100 * next_x, 100 * next_y, 100, 100))
+        angle = math.atan2(next_y + 0.5 - self.y, next_x + 0.5 - self.x)
+        dx = math.cos(angle) * self.speed
+        dy = math.sin(angle) * self.speed
+        self.check_wall_collision(dx, dy)
+
+    def check_wall(self, x, y):
+        return (x, y) not in self.game.map.world_map
+
+    def check_wall_collision(self, dx, dy):
+        if self.check_wall(int(self.x + dx * self.size), int(self.y)):
+            self.x += dx
+        if self.check_wall(int(self.x), int(self.y + dy * self.size)):
+            self.y += dy
+
+    def animate_death(self):
+        if not self.alive:
+            if self.game.global_trigger and self.frame_counter < len(self.death_images) - 1:
+                self.death_images.rotate(-1)
+                self.image = self.death_images[0]
+                self.frame_counter += 1
 
     def animate_pain(self):
         self.animate(self.pain_images)
@@ -38,15 +67,33 @@ class NPC(AnimatedSprite):
                 self.game.sound.npc_pain.play()
                 self.game.player.shot = False
                 self.pain = True
+                self.health -= self.game.weapon.damage
+                self.check_health()
+
+    def check_health(self):
+        if self.health < 1:
+            self.alive = False
+            self.game.sound.npc_death.play()
 
     def run_logic(self):
         if self.alive:
             self.ray_cast_value = self.ray_casting_player_npc()
             self.check_hit_in_npc()
+
             if self.pain:
                 self.animate_pain()
+
+            elif self.ray_cast_value:
+                self.player_search_trigger = True
+                self.animate(self.walk_images)
+                self.movement()
+            elif self.player_search_trigger:
+                self.animate(self.walk_images)
+                self.movement()
             else:
                 self.animate(self.idle_images)
+        else:
+            self.animate_death()
 
     @property
     def map_pos(self):
