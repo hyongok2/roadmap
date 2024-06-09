33374498cb7f4397313058fb8cf524e5ec27daee
Mun Hyeongog <64830380+hyongok2@users.noreply.github.com>
2024-06-09 18:20:01 +0900

    c# builder pattern faceted builder

---
+++ 05. Basic Theory/Design Pattern/csharp_design_pattern/builder pattern/BuilderPattern/FacetedBuilder/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacetedBuilder
+{
+    public class Person
+    {
+        // address
+        public string StreetAddress, Postcode, City;
+
+        // employment
+        public string CompanyName, Position;
+
+        public int AnnualIncome;
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetAddress)}: {StreetAddress}, {nameof(Postcode)}: {Postcode}, {nameof(City)}: {City}, {nameof(CompanyName)}: {CompanyName}, {nameof(Position)}: {Position}, {nameof(AnnualIncome)}: {AnnualIncome}";
+        }
+    }
+}
---
+++ 05. Basic Theory/Design Pattern/csharp_design_pattern/builder pattern/BuilderPattern/FacetedBuilder/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using FacetedBuilder;
+
+Person person = new PersonBuilder().Lives
+                                    .In("Korea").WithPostcode("11111").At("Seoul")
+                                   .Works
+                                    .At("Samsung").AsA("CEO").Earning(123000);
+
+Console.WriteLine(person);
