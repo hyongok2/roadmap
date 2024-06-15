2c5f34b6937bad8bba1b2ef8bb7670d19e72915b
hyongok2 <64830380+hyongok2@users.noreply.github.com>
2024-06-15 17:09:04 +0900

    C# async factory method

---
+++ 05. Basic Theory/Design Pattern/csharp_design_pattern/factory pattern/FactoryPattern/FactoryPattern/Foo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryPattern
+{
+    public class Foo
+    {
+        private Foo()
+        {
+
+        } 
+
+        public async Task<Foo> InitAsync()//여기에 필요한 비동기 작업을 추가한다.
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            Foo foo = new Foo();
+            return foo.InitAsync();
+        }
+    }
+}
---
+++ 05. Basic Theory/Design Pattern/csharp_design_pattern/factory pattern/FactoryPattern/FactoryPattern/Program.cs
@@ -0,0 +1,4 @@
+﻿
+using FactoryPattern;
+
+Foo x = await Foo.CreateAsync();
